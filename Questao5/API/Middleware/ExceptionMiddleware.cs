0725cc4e3fb571b4241618661c855f0e404cab13
Vinicius <viniciusmasson0@gmail.com>
2025-05-23 15:29:27 -0400

    feat(api): add Exception middleware

---
+++ Questao5/API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Questao5.Application.Exceptions;
+using Questao5.Domain.Exceptions;
+
+namespace Questao5.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro não tratado.");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            var statusCode = exception switch
+            {
+                NotFoundException => StatusCodes.Status404NotFound,
+                BusinessException => StatusCodes.Status400BadRequest,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            context.Response.StatusCode = statusCode;
+
+            var result = new
+            {
+                StatusCode = statusCode,
+                Message = exception.Message
+            };
+
+            return context.Response.WriteAsJsonAsync(result);
+        }
+    }
+}
