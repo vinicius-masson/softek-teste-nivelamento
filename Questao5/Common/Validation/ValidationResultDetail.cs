91335302da859d75b3c3b6c1ef787dc942aee188
Vinicius <viniciusmasson0@gmail.com>
2025-05-23 15:38:05 -0400

    feat(validation): add error detail model for standardized API responses

---
+++ Questao5/Common/Validation/ValidationResultDetail.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+
+namespace Questao5.Common.Validation
+{
+    public class ValidationResultDetail
+    {
+        public bool IsValid { get; set; }
+        public IEnumerable<ValidationErrorDetail> Errors { get; set; } = Array.Empty<ValidationErrorDetail>();
+
+        public ValidationResultDetail()
+        {
+
+        }
+
+        public ValidationResultDetail(ValidationResult validationResult)
+        {
+            IsValid = validationResult.IsValid;
+            Errors = validationResult.Errors.Select(o => (ValidationErrorDetail)o);
+        }
+    }
+}
