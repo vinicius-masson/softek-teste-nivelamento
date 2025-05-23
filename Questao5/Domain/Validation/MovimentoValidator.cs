6d510e1ea6f2d8dac22e4e48e04e90d4da6c6057
Vinicius <viniciusmasson0@gmail.com>
2025-05-23 15:19:49 -0400

    (domain): implement Movimento validator with FluentValidation

---
+++ Questao5/Domain/Validation/MovimentoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Questao5.Domain.Entities;
+
+namespace Questao5.Domain.Validation
+{
+    public class MovimentoValidator : AbstractValidator<Movimento>
+    {
+        public MovimentoValidator()
+        {
+            RuleFor(m => m.Valor)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage("INVALID_VALUE");
+
+            RuleFor(m => m.TipoMovimento)
+                .IsInEnum()
+                .WithMessage("INVALID_TYPE");
+        }
+    }
+}
