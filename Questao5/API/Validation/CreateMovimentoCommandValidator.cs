d03f65aef8b18703349e245e952e40cedb783381
Vinicius <viniciusmasson0@gmail.com>
2025-05-23 15:31:35 -0400

    feat(api): implement request validation

---
+++ Questao5/API/Validation/CreateMovimentoCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Questao5.Application.Commands.Movimentos;
+
+namespace Questao5.API.Validation
+{
+    public class CreateMovimentoCommandValidator : AbstractValidator<CreateMovimentoCommand>
+    {
+        public CreateMovimentoCommandValidator()
+        {
+            RuleFor(m => m.RequisicaoId)
+                .NotEmpty()
+                .WithMessage("Requisition Id is required.")
+                .Must(id => id != Guid.Empty && id != default(Guid))
+                .WithMessage("Requisition Id must be a valid GUID.");
+
+            RuleFor(m => m.IdContaCorrente)
+                .NotEmpty()
+                .WithMessage("Account Id is required.")
+                .Must(id => id != Guid.Empty && id != default(Guid))
+                .WithMessage("Account Id must be a valid GUID.");
+
+            RuleFor(m => m.Valor)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage("Valor must be greater than 0");
+
+            RuleFor(m => m.TipoMovimento)
+                .NotEmpty()
+                .WithMessage("TipoMovimento is required.")
+                .Must(t => t?.Trim().ToUpper() == "C" || t?.Trim().ToUpper() == "D")
+                .WithMessage("Movimento type must be 'C' (Credit) or 'D' (Debit).");
+        }
+    }
+}
---
+++ Questao5/API/Validation/GetContaQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Questao5.Application.Queries.Contas;
+
+namespace Questao5.API.Validation
+{
+    public class GetContaQueryValidator : AbstractValidator<GetContaQuery>
+    {
+        public GetContaQueryValidator()
+        {
+            RuleFor(c => c.IdContaCorrente)
+                .NotEmpty()
+                .WithMessage("Account Id is Required.")
+                .Must(id => id != Guid.Empty && id != default(Guid))
+                .WithMessage("Account Id must be a valid GUID.");
+        }
+    }
+}
