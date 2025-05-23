591a9fb6b8303d3df9c2f31c4735b7774e3db890
Vinicius <viniciusmasson0@gmail.com>
2025-05-23 15:27:54 -0400

    feat(application): add AutoMapper profile

---
+++ Questao5/Application/Profiles/MovimentoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Questao5.Application.Commands.Movimentos;
+using Questao5.Domain.Entities;
+using Questao5.Domain.Enumerators;
+namespace Questao5.Application.Profiles
+{
+    public class MovimentoProfile : Profile
+    {
+        public MovimentoProfile()
+        {
+            CreateMap<Movimento, CreateMovimentoResponse>();
+        }
+    }
+}
